455c738de2341d79d278c4da07b7a5c9092e2d6e
apradhn <abhijit.pradhan6@gmail.com>
2015-07-02 09:37:28 -0400

    all done, time to refactor

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_accessor :count
+
+  def initialize(count)
+    @count = count
+  end
+
+  def sum
+    first = 1
+    second = 2
+    fibs = [1, 2]
+    fib = 0
+    while fib < count
+      fib = first + second
+      fibs << fib if fib < count && fib.even?
+      first = second
+      second = fib
+    end
+    fibs.reject{|fib| fib == 1}.inject(0){|sum, fib| sum + fib}     
+  end
+  
+end
